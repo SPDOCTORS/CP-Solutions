bb88430b0a046e253e5dd84ef2e6313aa2c26b8e
SPDOCTORS <senthilkumar19769@gmail.com>
2026-08-26 22:41:53 +0530

    Add Shortest-and-Lexicographically-Smallest-Beautiful-String (sliding-window)

---
+++ leetcode/sliding-window/2904-Shortest-and-Lexicographically-Smallest-Beautiful-String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        positions = []
+
+        for i, ch in enumerate(s):
+            if ch == '1':
+                positions.append(i)
+
+        if len(positions) < k:
+            return ""
+
+        ans = ""
+
+        for i in range(len(positions) - k + 1):
+            start = positions[i]
+            end = positions[i + k - 1]
+
+            candidate = s[start:end + 1]
+
+            if ans == "":
+                ans = candidate
+            elif len(candidate) < len(ans):
+                ans = candidate
+            elif len(candidate) == len(ans) and candidate < ans:
+                ans = candidate
+
+        return ans
+        
