237cb35296716f7e0289f60dcc92f45085dab5b0
SPDOCTORS <senthilkumar19769@gmail.com>
2026-08-30 15:34:43 +0530

    Add Candy (array)

--- leetcode/array/135-Candy.py
+++ leetcode/array/135-Candy.py
@@ -4,15 +4,18 @@ class Solution:
         if n==0:
             return 0
         left=[1]*n
-        right=[1]*n
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
                 left[i]=left[i-1]+1
+        cur=1
+        right=1
+        sumi=max(1,left[n-1])
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
-                right[i]=right[i+1]+1
-        ans=0
-        for i in range(n):
-            ans+=max(left[i],right[i])
-        return ans
+                cur=right+1
+            else:
+                cur=1
+            right=cur
+            sumi+=max(left[i],cur)
+        return sumi
         
