51ad47f84aeb5597cb39db6da754af6adec6a615
SPDOCTORS <senthilkumar19769@gmail.com>
2026-08-30 16:26:15 +0530

    Add Candy (array)

--- leetcode/array/135-Candy.py
+++ leetcode/array/135-Candy.py
@@ -1,21 +1,24 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
-        if n==0:
-            return 0
-        left=[1]*n
-        for i in range(1,n):
-            if ratings[i]>ratings[i-1]:
-                left[i]=left[i-1]+1
-        cur=1
-        right=1
-        sumi=max(1,left[n-1])
-        for i in range(n-2,-1,-1):
-            if ratings[i]>ratings[i+1]:
-                cur=right+1
-            else:
-                cur=1
-            right=cur
-            sumi+=max(left[i],cur)
-        return sumi
+        i=1
+        sum=1
+        while i<n:
+            if ratings[i]==ratings[i-1]:
+                sum+=1
+                i+=1
+                continue
+            peak=1
+            while i<n and ratings[i]>ratings[i-1]:
+                peak+=1
+                sum+=peak
+                i+=1
+            down=1
+            while i<n and ratings[i]<ratings[i-1]:
+                sum+=down
+                i+=1
+                down+=1
+            if down>peak:
+                sum+=(down-peak)
+        return sum
         
