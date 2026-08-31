11c5f0fae53bb18984fb236925a1e237e694ad16
SPDOCTORS <senthilkumar19769@gmail.com>
2026-08-31 21:35:35 +0530

    Add Catch the Coin (800)

---
+++ codeforces/800/1989A - Catch the Coin.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int x,y;
+        cin>>x>>y;
+        if(y>=-1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
