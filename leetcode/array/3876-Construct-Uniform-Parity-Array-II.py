dcab5045c3f8e11e8ae69b5036ae48f472ec9e0f
SPDOCTORS <senthilkumar19769@gmail.com>
2026-09-03 08:49:27 +0530

    Add Construct-Uniform-Parity-Array-II (array)

--- leetcode/array/3876-Construct-Uniform-Parity-Array-II.py
+++ leetcode/array/3876-Construct-Uniform-Parity-Array-II.py
@@ -3,11 +3,11 @@ class Solution:
         it_even=False
         it_odd=False
         for x in nums1:
-            if x%2==1:
+            if (x&1)==1:
                 it_odd=True
             else:
                 it_even=True
         if not(it_even and it_odd):
             return True
-        return min(nums1)%2==1
+        return (min(nums1)&1)==1
         
