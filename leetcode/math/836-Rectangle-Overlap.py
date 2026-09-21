af74bd8f5b698603f6c8de4f2c5fc486a2e48264
SPDOCTORS <senthilkumar19769@gmail.com>
2026-09-21 18:40:01 +0530

    Add Rectangle-Overlap (math)

--- leetcode/math/836-Rectangle-Overlap.py
+++ leetcode/math/836-Rectangle-Overlap.py
@@ -1,10 +1,12 @@
 class Solution:
     def isRectangleOverlap(self, rec1: list[int], rec2: list[int]) -> bool:
-        if (rec1[0] >= rec2[2] or
-            rec1[1] >= rec2[3] or
-            rec2[0] >= rec1[2] or
-            rec2[1] >= rec1[3]):
-            return False
-
-        return True
+        x_start = max(rec1[0], rec2[0])
+        x_end = min(rec1[2], rec2[2])
+        width = x_end - x_start
+        y_start = max(rec1[1], rec2[1])
+        y_end = min(rec1[3], rec2[3])
+        height = y_end - y_start
+        if width>0 and height>0:
+            return True
+        return False
         
